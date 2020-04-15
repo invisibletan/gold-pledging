50cae8011e9da8398a25ac7dc51b50415569e8d4
it61070027 <icemanjuju@gmail.com>
2020-04-15 17:17:46 +0700

    view

--- web_gold/management/urls.py
+++ web_gold/management/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('' ,my_login, name='login'),
     path('pledging',pledging ,name='pledging'),
     path('customers', customers, name='customers'),
-    path('view_customer', view_customer, name='view_customer'),
+    path('view_customer/<int:cus_id>', view_customer, name='view_customer'),
+    path('view_pledging/<int:pled_id>', view_pledging, name='view_pledging')
 ]
--- web_gold/management/views.py
+++ web_gold/management/views.py
@@ -14,5 +14,10 @@ def customers(request):
     customer = Customer.objects.all()
     return render(request, 'customers.html', context={'customer': customer})
 
-def view_customer(request):
-    return render(request, 'view_customer.html')
+def view_customer(request, cus_id):
+    view_cus = Customer.objects.get(pk=cus_id)
+    return render(request, 'view_customer.html', context={'cus': view_cus})
+
+def view_pledging(request, pled_id):
+    view_pled = Pledging.objects.get(pk=pled_id)
+    return render(request, 'view_pledging.html', context={'p': view_pled})
